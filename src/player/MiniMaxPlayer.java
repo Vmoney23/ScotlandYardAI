c0db8bc5f5a5fe23e2679e7a5c3fedeab651865d
Ahmer Butt <ab15015@my.bristol.ac.uk>
2016-04-21 18:50:17 +0100

    store graph, skeleton for score

--- src/player/MiniMaxPlayer.java
+++ src/player/MiniMaxPlayer.java
@@ -1,8 +1,10 @@
 package player;
 
 import aigraph.*;
+import graph.UndirectedGraph;
 import scotlandyard.*;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -10,22 +12,40 @@ import java.util.*;
  */
 public class MiniMaxPlayer implements Player {
 
+    private int location;
     private ScotlandYardView currentGameState;
+    private UndirectedGraph<Integer, Transport> graph;
     private List<Move> moves;
 
     public MiniMaxPlayer(ScotlandYardView view, String graphFilename) {
         //TODO: A better AI makes use of `view` and `graphFilename`.
 
         this.currentGameState = view;
+        ScotlandYardGraphReader graphReader = new ScotlandYardGraphReader();
+        try {
+            this.graph = graphReader.readGraph("lib/scotlandyard.jar/" + graphFilename);
+        } catch (IOException e) {
+            System.err.println("failed to read " + graphFilename);
+        }
     }
 
+
+    /**
+     *
+     * @param location
+     * @param moves
+     * @param token
+     * @param receiver
+     */
     @Override
-    public void notify(int location, List<Move> moves, Integer token, Receiver receiver) {
+    public void notify(int location, List<Move> moves, Integer token,
+                       Receiver receiver) {
         // update current game state
         if (!(receiver instanceof ScotlandYardView))
             throw new IllegalArgumentException("Receiver must implement ScotlandYardView");
-        currentGameState = (ScotlandYardView) receiver;
+        this.currentGameState = (ScotlandYardView) receiver;
         this.moves = moves;
+        this.location = location;
 
         // get ai move
         System.out.println("Getting move");
@@ -43,14 +63,44 @@ public class MiniMaxPlayer implements Player {
      */
     Move getAIMove() {
         // create new game tree to specified depth, with root as current state of the game
-        int treeDepth = 0;
-        ScotlandYardGameTree gameTree = new ScotlandYardGameTree(currentGameState);
-        generateTree(gameTree, treeDepth);//calls pruneTree(), score()
-
+        //int treeDepth = 0;
+        //ScotlandYardGameTree gameTree = new ScotlandYardGameTree(currentGameState);
+        //generateTree(gameTree, treeDepth);//calls pruneTree(), score()
+        HashMap<Move, Integer> moveScores = score();
         // return best moved based on MiniMax
         //List<AINode<ScotlandYardView>> finalStates = gameTree.getFinalStatesList();
         //return minimax(finalStates); // TODO HOW TO GET INITIAL MOVE?? map scores/states->initial move?
         return moves.get(0);
+    }
+
+    /**
+     * Calculates scores for all given moves.
+     *
+     * @return map of each move to its score.
+     */
+    private HashMap<Move, Integer> score() {
+        // create map of moves to scores
+        HashMap<Move, Integer> moveScoreMap = new HashMap<>();
+
+        // store locations of other players
+        HashMap<Colour, Integer> playerLocationMap = new HashMap<>();
+        for (Colour player : currentGameState.getPlayers()) {
+            playerLocationMap.put(player, currentGameState.getPlayerLocation(player));
+        }
+
+        // iterate through possible moves, calculating score for each one and
+        // adding to moveScoreMap
+        for (Move move : moves) {
+            int score = 0;
+
+            // TODO score a double move
+            // TODO score a single move
+            if (move instanceof MoveTicket) {
+
+            }
+        }
+
+        return moveScoreMap;
     }
 
     /**
@@ -63,7 +113,7 @@ public class MiniMaxPlayer implements Player {
     }
 
 
-    private Move minimax(List<AINode<ScotlandYardView>> gameStates) {
+    private static Move minimax(List<AINode<ScotlandYardView>> gameStates) {
         return null;
     }
 
