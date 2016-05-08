6b53185cec33d8560d421d8b63479e826b368032
Ahmer Butt <ab15015@my.bristol.ac.uk>
2016-05-08 14:00:01 +0100

    changed depth to 4

--- src/player/MiniMaxPlayer.java
+++ src/player/MiniMaxPlayer.java
@@ -104,7 +104,7 @@ public class MiniMaxPlayer implements Player {
 
         // calculate a score for each move by using the MiniMax algorithm.
         // return the move with the best score.
-        int depth = 3;
+        int depth = 4;
         boolean mrx = colour.equals(Colour.Black);
         return score(depth, mrx);
     }
